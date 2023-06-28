b11cc087f93b8a74f9658563503417352693b9ea
Pablog44 <137789716+Pablog44@users.noreply.github.com>
2023-06-28 04:54:48 +0200

    Add files via upload

--- dinocuadrado/game.js
+++ dinocuadrado/game.js
@@ -1,9 +1,10 @@
 let player = document.getElementById("player");
 let obstacle = document.getElementById("obstacle");
 let gameover = false;
+let ducking = false;
 
 function jump() {
-    if (player.classList != "animate") {
+    if (!ducking && player.classList != "animate") {
         player.classList.add("animate");
     }
     setTimeout(() => {
@@ -11,16 +12,38 @@ function jump() {
     }, 500);
 }
 
+function duck() {
+    player.classList.add('duck');
+    ducking = true;
+    setTimeout(() => {
+        player.classList.remove('duck');
+        ducking = false;
+    }, 1000);
+}
+
 let checkDead = setInterval(() => {
     let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    let obstacleHeight = parseInt(window.getComputedStyle(obstacle).getPropertyValue("height"));
 
-    if(obstacleLeft < 50 && obstacleLeft > 0 && playerTop >= 150){
+    if(obstacleLeft < 50 && obstacleLeft > 0 && playerTop >= 150 && obstacleHeight > 30) {
         obstacle.style.animation = "none";
         gameover = true;
         alert("Game Over. Retry?");
     }
 }, 10);
+
+document.getElementById('jumpBtn').addEventListener('click', function() {
+    if (!gameover) {
+        jump();
+    }
+});
+
+document.getElementById('duckBtn').addEventListener('click', function() {
+    if (!gameover) {
+        duck();
+    }
+});
 
 window.addEventListener('keydown', function (e) {
     if(e.code == "Space" && !gameover){
@@ -35,3 +58,11 @@ document.body.addEventListener('touchstart', function (e) {
         jump();
     }
 });
+
+setInterval(function() {
+    if (Math.random() < 0.5) {
+        obstacle.style.height = "50px";
+    } else {
+        obstacle.style.height = "25px";
+    }
+}, 2000);
