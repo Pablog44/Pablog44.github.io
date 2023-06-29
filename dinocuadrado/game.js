d4e27874e2c7eedb829de53ac869d46c5c540048
Pablog44 <137789716+Pablog44@users.noreply.github.com>
2023-06-29 19:12:28 +0200

    Add files via upload

--- dinocuadrado/game.js
+++ dinocuadrado/game.js
@@ -21,48 +21,59 @@ function duck() {
     }, 1000);
 }
 
+function reset() {
+    obstacle.style.animation = "move 2s infinite linear";
+    gameover = false;
+}
+
 let checkDead = setInterval(() => {
     let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
     let obstacleHeight = parseInt(window.getComputedStyle(obstacle).getPropertyValue("height"));
+    let obstacleBottom = parseInt(window.getComputedStyle(obstacle).getPropertyValue("bottom"));
 
-    if(obstacleLeft < 50 && obstacleLeft > 0 && playerTop >= 150 && obstacleHeight > 30) {
+    if (obstacleLeft < 50 && obstacleLeft > 0 && playerTop >= 150 - obstacleBottom && playerTop < 200 - obstacleBottom) {
         obstacle.style.animation = "none";
         gameover = true;
-        alert("Game Over. Retry?");
+        if (confirm("Game Over. Retry?")) {
+            reset();
+        }
     }
 }, 10);
 
-document.getElementById('jumpBtn').addEventListener('click', function() {
+document.getElementById('jumpBtn').addEventListener('click', function () {
     if (!gameover) {
         jump();
     }
 });
 
-document.getElementById('duckBtn').addEventListener('click', function() {
+document.getElementById('duckBtn').addEventListener('click', function () {
     if (!gameover) {
         duck();
     }
 });
 
 window.addEventListener('keydown', function (e) {
-    if(e.code == "Space" && !gameover){
+    if (e.code == "Space" && !gameover) {
         e.preventDefault();
         jump();
     }
 });
 
 document.body.addEventListener('touchstart', function (e) {
-    if(!gameover){
+    if (!gameover) {
         e.preventDefault();
         jump();
     }
 });
 
-setInterval(function() {
+setInterval(function () {
+    obstacle.style.height = "50px";
     if (Math.random() < 0.5) {
-        obstacle.style.height = "50px";
+        obstacle.style.bottom = "0px";
+        obstacle.classList.remove('lower');
     } else {
-        obstacle.style.height = "25px";
+        obstacle.style.bottom = "30px";
+        obstacle.classList.add('lower');
     }
 }, 2000);
