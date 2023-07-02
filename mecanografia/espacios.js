e283186c766dfa16610ec7bcd5856def75ee3e05
Pablog44 <137789716+Pablog44@users.noreply.github.com>
2023-07-02 04:53:02 +0200

    Add files via upload

--- mecanografia/espacios.js
+++ mecanografia/espacios.js
@@ -1,61 +1,120 @@
-const alphabet = 'abcdefghijklmnñopqrstuvwxyz-.,';
-let generatedText = '';
-let correctCount = 0;
-let wrongCount = 0;
+const areaMostrar = document.getElementById("displayArea");
+const areaTipear = document.getElementById("typingArea");
+const areaPuntuacion = document.getElementById("scoreArea");
+const botonReiniciar = document.getElementById("resetBtn");
+const areaTiempo = document.getElementById("timeArea");
+const nuevaAreaTexto = document.getElementById("newTextArea");
+const botonTextoNuevo = document.getElementById("newTextBtn");
 
-const displayArea = document.getElementById('displayArea');
-const typingArea = document.getElementById('typingArea');
-const scoreArea = document.getElementById('scoreArea');
-const resetBtn = document.getElementById('resetBtn');
+let textoActual = "";
+let cuentaCorrectas = 0;
+let cuentaIncorrectas = 0;
+let tiempoInicio;
+let totalCaracteres = 0;
+let intervaloTemporizador;
+
+let palabras = [
+    "hola", "que", "tal", "como", "esta", "yo", "muy", "bien", "tu", "otro", "dia", "soleado", "calor", "frio",
+    "fuego", "hielo", "detras", "delante", "arriba", "abajo", "champu", "cepillo", "coleta", "nublado","vida",
+    "salud","mundo","amor","odio","felicidad","tristeza","alegría","envidia","miedo","esperanza","sueño",
+    "viaje","cine","libro","música","juego","deporte","naturaleza","mar","sol","luna","cielo","estrella",
+    "ciudad","país","casa","perro","gato","auto","comida","agua","aire","fuego","tierra","dinero",
+    "amigo","familia","trabajo",
+    "piedra", "papel", "tijera", "rosa", "margarita", "cactus", "nube", "tormenta", "relámpago", "café", 
+    "té", "azúcar", "sal", "pimienta", "bocadillo", "queso", "pan", "leche", "uva", "manzana", "naranja", 
+    "plátano", "kiwi", "morado", "azul", "verde", "amarillo", "rojo", "blanco", "negro", "gris", "rosado",
+    "cortina", "puerta", "ventana", "escalera", "silla", "sofá", "cama", "almohada", "edredón", "pintura",
+    "retrato", "guitarra", "piano", "batería", "saxofón", "violín", "flauta", "arco", "flecha", "escudo",
+    "espada", "casco", "armadura", "dragón", "hada", "sirena", "unicornio", "duende", "bruja", "vampiro"
+];
+
+function obtenerNuevaPalabra() {
+    const indice = Math.floor(Math.random() * palabras.length);
+    return palabras[indice];
+}
+
+function generarPalabras() {
+    palabrasActuales = [];
+    for (let i = 0; i < 20; i++) {
+        palabrasActuales.push(obtenerNuevaPalabra());
+    }
+    textoActual = palabrasActuales.join(" ");
+    areaMostrar.innerText = textoActual;
+}
+
+botonTextoNuevo.addEventListener('click', function() {
+    if (nuevaAreaTexto.value.length !== 0) {
+        palabras = nuevaAreaTexto.value.split(" ");
+        nuevaAreaTexto.value = '';
+    }
+    reiniciarJuego();
+    areaTipear.focus();
+});
+
+areaTipear.addEventListener("input", function(e) {
+    const caracterTipeado = e.data;
+    totalCaracteres++;
+
+    if (caracterTipeado === ' ') {
+        if (textoActual[0] === ' ') {
+            textoActual = textoActual.substring(1);
+            cuentaCorrectas++;
+        } else {
+            cuentaIncorrectas++;
+        }
+    } else if (caracterTipeado === textoActual[0]) {
+        textoActual = textoActual.substring(1);
+        cuentaCorrectas++;
+    } else {
+        cuentaIncorrectas++;
+    }
+
+    areaMostrar.innerText = textoActual;
+    areaPuntuacion.textContent = 'Aciertos: ' + cuentaCorrectas + ' / Fallos: ' + cuentaIncorrectas;
+    areaTipear.value = '';
+
+    if (textoActual.length === 0) {
+        mostrarPulsacionesPorMinuto();
+        clearInterval(intervaloTemporizador);
+    }
+});
+
+botonReiniciar.addEventListener('click', function() {
+    mostrarPulsacionesPorMinuto();
+    reiniciarJuego();
+});
+
+function reiniciarJuego() {
+    cuentaCorrectas = 0;
+    cuentaIncorrectas = 0;
+    totalCaracteres = 0;
+    generarPalabras();
+    areaPuntuacion.textContent = 'Aciertos: 0 / Fallos: 0';
+    areaTiempo.textContent = '';
+    tiempoInicio = new Date();
+    areaTipear.value = '';
+
+    if (intervaloTemporizador) {
+        clearInterval(intervaloTemporizador);
+    }
+
+    intervaloTemporizador = setInterval(function() {
+        let ahora = new Date();
+        let tiempoTranscurrido = (ahora - tiempoInicio) / 1000;
+        areaTiempo.innerText = 'Tiempo: ' + tiempoTranscurrido.toFixed(1) + ' segundos';
+    }, 100);
+}
+
+function mostrarPulsacionesPorMinuto() {
+    let ahora = new Date();
+    let tiempoTranscurrido = (ahora - tiempoInicio) / 1000;
+
+    let netoCorrectos = cuentaCorrectas;
+    let pulsacionesPorMinuto = Math.floor((netoCorrectos / tiempoTranscurrido) * 60);
+    areaPuntuacion.innerText = 'Aciertos: ' + cuentaCorrectas + ' / Fallos: ' + cuentaIncorrectas + ' / Ppm: ' + pulsacionesPorMinuto;
+}
 
 window.onload = function() {
-    generateText(15);
-    displayText();
-    typingArea.focus();  
+    reiniciarJuego();
+    areaTipear.focus();
 }
-
-function generateCharacter() {
-    if (Math.random() < 0.15) { // genera un espacio aproximadamente cada 7 caracteres
-        return ' ';
-    } else {
-        return alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
-}
-
-function generateText(length) {
-    for (let i = 0; i < length; i++) {
-        generatedText += generateCharacter();
-    }
-}
-
-function displayText() {
-    displayArea.value = generatedText;
-}
-
-typingArea.addEventListener('keydown', function checkInput(event) {
-    const currentKey = event.key;
-    const currentChar = generatedText[0];
-
-    if (currentKey === currentChar) {
-        correctCount++;
-        generatedText = generatedText.slice(1);
-        generatedText += generateCharacter();
-        displayText();
-        typingArea.value = '';
-    } else {
-        wrongCount++;
-    }
-
-    scoreArea.textContent = 'Aciertos: ' + correctCount + ' / Fallos: ' + wrongCount;
-});
-
-resetBtn.addEventListener('click', function resetText() {
-    generatedText = '';
-    typingArea.value = '';
-    correctCount = 0;
-    wrongCount = 0;
-    scoreArea.textContent = 'Aciertos: ' + correctCount + ' / Fallos: ' + wrongCount;
-    generateText(15);
-    displayText();
-    typingArea.focus();
-});
--- mecanografia/tutexto.js
+++ mecanografia/tutexto.js
@@ -10,63 +10,52 @@ let textoActual = "";
 let cuentaCorrectas = 0;
 let cuentaIncorrectas = 0;
 let tiempoInicio;
-let totalCaracteres = 0;
 let intervaloTemporizador;
 
-let palabras = [
-    /* tus palabras aquí */
-];
-
-function obtenerNuevaPalabra() {
-    const indice = Math.floor(Math.random() * palabras.length);
-    return palabras[indice];
-}
-
-function generarPalabras() {
-    palabrasActuales = [];
-    for (let i = 0; i < 20; i++) {
-        palabrasActuales.push(obtenerNuevaPalabra());
-    }
-    textoActual = palabrasActuales.join(" ");
-    areaMostrar.innerText = textoActual;
-}
-
 botonTextoNuevo.addEventListener('click', function() {
-    if (nuevaAreaTexto.value.length !== 0) {
-        palabras = nuevaAreaTexto.value.split(" ");
+    if (textoActual.length === 0 && nuevaAreaTexto.value.length !== 0) {
+        textoActual = nuevaAreaTexto.value;
+        areaMostrar.innerText = textoActual;
         nuevaAreaTexto.value = '';
+        // Enfocarse en el área de tipeo para abrir el teclado en dispositivos móviles.
+        areaTipear.focus();
+        // Inicio del tiempo cuando se presiona enter y se tienen palabras para procesar
+        tiempoInicio = new Date();
+        if (intervaloTemporizador) {
+            clearInterval(intervaloTemporizador);
+        }
+        intervaloTemporizador = setInterval(function() {
+            let ahora = new Date();
+            let tiempoTranscurrido = (ahora - tiempoInicio) / 1000;
+            areaTiempo.innerText = 'Tiempo: ' + tiempoTranscurrido.toFixed(1) + ' segundos';
+        }, 100);
     }
-    reiniciarJuego();
-    areaTipear.focus();
 });
 
 areaTipear.addEventListener("input", function(e) {
     const caracterTipeado = e.data;
-    totalCaracteres++;
 
-    if (caracterTipeado === ' ') {
-        if (textoActual[0] === ' ') {
-            textoActual = textoActual.substring(1);
-            cuentaCorrectas++;
-        } else {
-            cuentaIncorrectas++;
-        }
-    } else if (caracterTipeado === textoActual[0]) {
-        textoActual = textoActual.substring(1);
+    if (caracterTipeado === textoActual.charAt(0)) {
+        textoActual = textoActual.slice(1);
         cuentaCorrectas++;
     } else {
-        cuentaIncorrectas++;
+        if (caracterTipeado !== ' ') { // Ignorar los espacios adicionales introducidos
+            cuentaIncorrectas++;
+        }
     }
 
     areaMostrar.innerText = textoActual;
     areaPuntuacion.textContent = 'Aciertos: ' + cuentaCorrectas + ' / Fallos: ' + cuentaIncorrectas;
-    areaTipear.value = '';
 
+    // Verificar si todas las palabras han sido completadas
     if (textoActual.length === 0) {
         mostrarPulsacionesPorMinuto();
         clearInterval(intervaloTemporizador);
     }
 });
+
+
+
 
 botonReiniciar.addEventListener('click', function() {
     mostrarPulsacionesPorMinuto();
@@ -74,32 +63,21 @@ botonReiniciar.addEventListener('click', function() {
 });
 
 function reiniciarJuego() {
+    textoActual = "";
     cuentaCorrectas = 0;
     cuentaIncorrectas = 0;
-    totalCaracteres = 0;
-    generarPalabras();
     areaPuntuacion.textContent = 'Aciertos: 0 / Fallos: 0';
     areaTiempo.textContent = '';
-    tiempoInicio = new Date();
     areaTipear.value = '';
-
-    if (intervaloTemporizador) {
-        clearInterval(intervaloTemporizador);
-    }
-
-    intervaloTemporizador = setInterval(function() {
-        let ahora = new Date();
-        let tiempoTranscurrido = (ahora - tiempoInicio) / 1000;
-        areaTiempo.innerText = 'Tiempo: ' + tiempoTranscurrido.toFixed(1) + ' segundos';
-    }, 100);
+    nuevaAreaTexto.value = '';
+    areaTipear.focus();
 }
 
 function mostrarPulsacionesPorMinuto() {
     let ahora = new Date();
-    let tiempoTranscurrido = (ahora - tiempoInicio) / 1000;
+    let tiempoTranscurrido = (ahora - tiempoInicio) / 60000; // Tiempo en minutos
 
-    let netoCorrectos = cuentaCorrectas;
-    let pulsacionesPorMinuto = Math.floor((netoCorrectos / tiempoTranscurrido) * 60);
+    let pulsacionesPorMinuto = Math.floor(cuentaCorrectas / tiempoTranscurrido);
     areaPuntuacion.innerText = 'Aciertos: ' + cuentaCorrectas + ' / Fallos: ' + cuentaIncorrectas + ' / Ppm: ' + pulsacionesPorMinuto;
 }
 
