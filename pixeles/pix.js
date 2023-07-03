ca780123a94e179f6b3e0d03f393257ab6549bc7
Pablog44 <137789716+Pablog44@users.noreply.github.com>
2023-07-03 03:33:37 +0200

    Add files via upload

--- pixeles/pix.js
+++ pixeles/pix.js
@@ -1,9 +1,11 @@
 const imageInput = document.getElementById('image-input');
 const sourceCanvas = document.getElementById('source-canvas');
 const outputCanvas = document.getElementById('output-canvas');
-const ctxSource = sourceCanvas.getContext('2d');
-const ctxOutput = outputCanvas.getContext('2d');
+const sourceCtx = sourceCanvas.getContext('2d');
+const outputCtx = outputCanvas.getContext('2d');
 const downloadBtn = document.getElementById('download-btn');
+const colorPalette = document.getElementById('color-palette');
+let selectedColor = 'red';
 
 imageInput.addEventListener('change', function(e) {
     const file = e.target.files[0];
@@ -12,25 +14,42 @@ imageInput.addEventListener('change', function(e) {
     reader.onload = function(e) {
         const img = new Image();
         img.onload = function() {
-            const pixelSize = 10;
-            const gridSize = 1;
-            const size = 100 * (pixelSize + gridSize);
             sourceCanvas.width = 100;
             sourceCanvas.height = 100;
-            outputCanvas.width = size;
-            outputCanvas.height = size;
-            ctxSource.drawImage(img, 0, 0, 100, 100);
-            for(let y = 0; y < 100; y++) {
+            outputCanvas.width = 1000;
+            outputCanvas.height = 1000;
+
+            sourceCtx.drawImage(img, 0, 0, 100, 100);
+
+            for (let y = 0; y < 100; y++) {
                 for(let x = 0; x < 100; x++) {
-                    const pixelData = ctxSource.getImageData(x, y, 1, 1).data;
-                    ctxOutput.fillStyle = `rgba(${pixelData[0]},${pixelData[1]},${pixelData[2]},${pixelData[3]/255})`;
-                    ctxOutput.fillRect(x * (pixelSize + gridSize), y * (pixelSize + gridSize), pixelSize, pixelSize);
+                    const pixelData = sourceCtx.getImageData(x, y, 1, 1).data;
+                    outputCtx.fillStyle = `rgba(${pixelData[0]},${pixelData[1]},${pixelData[2]},${pixelData[3]/255})`;
+                    outputCtx.fillRect(x*10, y*10, 10, 10);
                 }
             }
         }
         img.src = e.target.result;
     }
     reader.readAsDataURL(file);
+});
+
+outputCanvas.addEventListener('click', function(e) {
+    const rect = outputCanvas.getBoundingClientRect();
+    const scaleX = outputCanvas.width / rect.width;
+    const scaleY = outputCanvas.height / rect.height;
+    const x = Math.floor(e.offsetX * scaleX / 10);
+    const y = Math.floor(e.offsetY * scaleY / 10);
+    outputCtx.fillStyle = selectedColor;
+    outputCtx.fillRect(x*10, y*10, 10, 10);
+});
+
+colorPalette.addEventListener('click', function(e) {
+    if (e.target.className.includes('color')) {
+        document.querySelector('.selected').classList.remove('selected');
+        e.target.classList.add('selected');
+        selectedColor = e.target.getAttribute('data-color');
+    }
 });
 
 downloadBtn.addEventListener('click', function() {
