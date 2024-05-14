3cccc87866be106cf6f52d9672726d7533344c05
Pablog44 <137789716+Pablog44@users.noreply.github.com>
2024-05-14 04:48:00 +0200

    Add files via upload

---
+++ gym3/ajuste.js
@@ -0,0 +1,146 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getFirestore, collection, getDocs, doc, getDoc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8sWu3ZG6NWvlEKQHRi23c7CgPPy_6yag",
+    authDomain: "apuntagym.firebaseapp.com",
+    projectId: "apuntagym",
+    storageBucket: "apuntagym.appspot.com",
+    messagingSenderId: "522093591127",
+    appId: "1:522093591127:web:27e2e56085d50c85b18112",
+    measurementId: "G-PY9MT94G92"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+const muscleGroupSelect = document.getElementById('muscle-group');
+const exerciseSelect = document.getElementById('exercise');
+const deleteMuscleGroupButton = document.getElementById('delete-muscle-group');
+const deleteExerciseButton = document.getElementById('delete-exercise');
+const debugInfo = document.getElementById('debug-info');
+
+let currentUser;
+
+onAuthStateChanged(auth, user => {
+    if (!user) {
+        window.location.href = 'inicio.html';
+    } else {
+        currentUser = user;
+        initializeMuscleGroups();
+    }
+});
+
+document.getElementById('logout-button').addEventListener('click', () => {
+    signOut(auth).then(() => {
+        window.location.href = 'inicio.html';
+    }).catch(error => {
+        console.error('Error al cerrar sesión:', error);
+    });
+});
+
+function initializeMuscleGroups() {
+    const muscleGroupsRef = collection(db, "muscleGroups");
+    const userMuscleGroupsRef = collection(db, "userMuscleGroups");
+    
+    Promise.all([getDocs(muscleGroupsRef), getDocs(userMuscleGroupsRef)]).then(([muscleGroupsSnapshot, userMuscleGroupsSnapshot]) => {
+        muscleGroupSelect.innerHTML = ''; // Limpiar las opciones existentes
+        
+        muscleGroupsSnapshot.forEach(doc => {
+            const option = document.createElement("option");
+            option.textContent = doc.id;
+            option.value = doc.id;
+            muscleGroupSelect.appendChild(option);
+        });
+        
+        userMuscleGroupsSnapshot.forEach(doc => {
+            if (doc.data().userId === currentUser.uid) {
+                const option = document.createElement("option");
+                option.textContent = doc.id.replace("Personalizado-", "");
+                option.value = `${doc.id} (Personalizado)`;
+                muscleGroupSelect.appendChild(option);
+            }
+        });
+
+        updateExerciseOptions();
+    }).catch(error => {
+        console.error("Error cargando grupos musculares:", error);
+        debugInfo.innerText = "Error cargando grupos musculares: " + error;
+    });
+}
+
+function updateExerciseOptions() {
+    const selectedGroup = muscleGroupSelect.value.replace(" (Personalizado)", "").replace("Personalizado-", "");
+    const isCustomGroup = muscleGroupSelect.value.includes(" (Personalizado)");
+    const groupRef = isCustomGroup ? doc(db, "userMuscleGroups", `Personalizado-${selectedGroup}`) : doc(db, "muscleGroups", selectedGroup);
+
+    getDoc(groupRef).then(docSnap => {
+        if (docSnap.exists()) {
+            const data = docSnap.data();
+            exerciseSelect.innerHTML = ''; // Limpiar las opciones anteriores
+            data.exercises.forEach(exercise => {
+                const option = document.createElement("option");
+                option.value = exercise;
+                option.textContent = exercise;
+                exerciseSelect.appendChild(option);
+            });
+        } else {
+            console.log(`No se encontraron ejercicios para el grupo ${selectedGroup}`);
+            debugInfo.innerText = "No se encontraron ejercicios para el grupo seleccionado.";
+        }
+    }).catch(error => {
+        console.error("Error cargando ejercicios:", error);
+        debugInfo.innerText = "Error cargando ejercicios: " + error;
+    });
+}
+
+function deleteMuscleGroup() {
+    const muscleGroup = muscleGroupSelect.value.replace(" (Personalizado)", "").replace("Personalizado-", "");
+    const isCustomGroup = muscleGroupSelect.value.includes(" (Personalizado)");
+    const groupRef = isCustomGroup ? doc(db, "userMuscleGroups", `Personalizado-${muscleGroup}`) : doc(db, "muscleGroups", muscleGroup);
+
+    if (confirm(`¿Estás seguro de que deseas eliminar el grupo muscular ${muscleGroup}?`)) {
+        deleteDoc(groupRef).then(() => {
+            console.log(`Grupo muscular ${muscleGroup} eliminado.`);
+            debugInfo.innerText = `Grupo muscular ${muscleGroup} eliminado.`;
+            initializeMuscleGroups();
+        }).catch(error => {
+            console.error(`Error eliminando el grupo muscular ${muscleGroup}:`, error);
+            debugInfo.innerText = `Error eliminando el grupo muscular ${muscleGroup}: ${error}`;
+        });
+    }
+}
+
+function deleteExercise() {
+    const exercise = exerciseSelect.value;
+    const muscleGroup = muscleGroupSelect.value.replace(" (Personalizado)", "").replace("Personalizado-", "");
+    const isCustomGroup = muscleGroupSelect.value.includes(" (Personalizado)");
+    const groupRef = isCustomGroup ? doc(db, "userMuscleGroups", `Personalizado-${muscleGroup}`) : doc(db, "muscleGroups", muscleGroup);
+
+    if (confirm(`¿Estás seguro de que deseas eliminar el ejercicio ${exercise} del grupo muscular ${muscleGroup}?`)) {
+        getDoc(groupRef).then(docSnap => {
+            if (docSnap.exists()) {
+                const exercises = docSnap.data().exercises;
+                const updatedExercises = exercises.filter(ex => ex !== exercise);
+
+                updateDoc(groupRef, { exercises: updatedExercises }).then(() => {
+                    console.log(`Ejercicio ${exercise} eliminado del grupo muscular ${muscleGroup}.`);
+                    debugInfo.innerText = `Ejercicio ${exercise} eliminado del grupo muscular ${muscleGroup}.`;
+                    updateExerciseOptions();
+                }).catch(error => {
+                    console.error(`Error eliminando el ejercicio ${exercise} del grupo muscular ${muscleGroup}:`, error);
+                    debugInfo.innerText = `Error eliminando el ejercicio ${exercise} del grupo muscular ${muscleGroup}: ${error}`;
+                });
+            }
+        }).catch(error => {
+            console.error(`Error obteniendo el grupo muscular ${muscleGroup}:`, error);
+            debugInfo.innerText = `Error obteniendo el grupo muscular ${muscleGroup}: ${error}`;
+        });
+    }
+}
+
+muscleGroupSelect.addEventListener('change', updateExerciseOptions);
+deleteMuscleGroupButton.addEventListener('click', deleteMuscleGroup);
+deleteExerciseButton.addEventListener('click', deleteExercise);
